9f4364ff013ed39cff9ea5c50ec852f228cf0f44
kage0 <dndjyagami@gmail.com>
2021-06-29 15:11:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let balls = document.getElementsByClassName("ball");
+let eye = document.getElementsByClassName("eye");
+
+document.onmousemove = function (e) {
+  let x = (e.clientX * 100) / innerWidth + "%";
+  let y = (e.clientY * 100) / innerHeight + "%";
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = `translate(-${x}, -${y})`;
+  }
+
+  for (let i = 0; i < 2; i++) {
+    eye[i].addEventListener("click", (e) => {
+      console.log("click");
+      e.target.classList.add("close-eye");
+      console.log(`${e.target.classList}`);
+    });
+  }
+
+  console.log("innerWidth: ", innerWidth);
+  console.log("innerHeight: ", innerHeight);
+  console.log("x = ", x);
+  console.log("y = ", y);
+};
+
+array.forEach((element, index) => {});
+
+
+  Je suis le plus grand 
